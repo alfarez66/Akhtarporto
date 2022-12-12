604aa17c576a6f849adada72679826b4d8041a7a
alfarez66 <79447464+alfarez66@users.noreply.github.com>
2022-12-12 12:56:01 +0700

    Initial commit

---
+++ assets/js/Navi.js
@@ -0,0 +1,12 @@
+let Navi = `
+<div>
+    <p>Profile | Akhtar Alfarisi Sriyanto</p>
+</div>
+<nav>
+    <ul class="navigation" >
+        <li id="A"><a href="../html/home.html"style="text-decoration: none; color:#FBF9FA">Home</a></li>
+        <li id="B"><a href="../html/About.html"style="text-decoration: none; color:#FBF9FA">About</a></li>
+        <li id="C"><a href="../html/contact.html"style="text-decoration: none; color:#FBF9FA">Contact</a></li>
+    </ul>
+</nav>`
+document.getElementById("Navi").innerHTML = Navi
